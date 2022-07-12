25466482d4fb7bb7d68d89c36dfda65c93bbcc47
xstartstaticx <daniel.reiller@icloud.com>
2022-07-12 16:38:01 +0200

    finished exercise

---
+++ solution.js
@@ -0,0 +1,23 @@
+// 1
+let i = 0;
+while (i < 15) {
+  console.log(i);
+  i++;
+}
+
+// 2
+i = 1;
+sum = 0;
+
+while (i <= 20) {
+  sum += i;
+  i++;
+}
+console.log(sum);
+
+// 3
+i = 0;
+do {
+  console.log("The number is", i);
+  i++;
+} while (i < 20);
